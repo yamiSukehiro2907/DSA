b90c24b22c48bed60e2240bb568b25e4276dc7eb
Vimal Kumar Yadav <vimalyadavkr001@gmail.com>
2025-03-19 00:10:20 +0530

    changes needed to be done currently busy so, later

--- L21/LRU_singly.java
+++ L21/LRU_singly.java
@@ -5,68 +5,69 @@ import java.util.*;
 class SongNode {
     String data;
     SongNode next;
-    SongNode prev;
 
     SongNode(String data) {
         this.data = data;
         this.next = null;
-        this.prev = null;
     }
 }
 
 class Spotify {
-    private SongNode head;
+    private SongNode LRU;
+    private SongNode MRU;
     private int capacity;
     private int size;
-    private Set<String> set;
+    private Map<String, SongNode> map;
 
     Spotify(int capacity) {
         this.capacity = capacity;
-        this.head = null;
+        this.LRU = null;
+        this.MRU = null;
         this.size = 0;
-        this.set = new HashSet<>();
+        this.map = new HashMap<>();
     }
 
     public void insert(String song) {
-        boolean exist = set.contains(song);
-        if (exist) {
-            if (head.data == song)
-                return;
-            SongNode prev = head;
-            SongNode curr = head.next;
-            while (curr != null && curr.data != song) {
-                curr = curr.next;
-                prev = prev.next;
-            }
-            prev.next = curr.next;
-            insertAtHead(song);
+        if (size == 0) {
+            SongNode newHead = new SongNode(song);
+            newHead.next = LRU;
+            map.put(song, null);
+            LRU = newHead;
+            MRU = newHead;
             return;
         }
-        insertAtHead(song);
-        set.add(song);
-        size++;
-        if (size > capacity) {
-            SongNode temp = head;
-            while (temp != null && temp.next != null && temp.next.next != null)
-                temp = temp.next;
-            set.remove(temp.data);
-            temp.next = null;
+        boolean exist = map.containsKey(song);
+        if (exist) {
+            if (size < capacity) {
+                if (MRU.data == song)
+                    return;
+                if (LRU.data == song) {
+                    LRU = LRU.next;
+                    insertAtMRU(song);
+                    return;
+                }
+                SongNode previous = map.get(song);
+                SongNode next = previous.next.next;
+                previous.next = next;
+                insertAtMRU(song);
+            }
         }
-        return;
+        if (size < capacity) {
+            map.put(song, MRU);
+            insertAtMRU(song);
+            size++;
+            return;
+        }
     }
 
-    public void insertAtHead(String song) {
+    private void insertAtMRU(String song) {
         SongNode newHead = new SongNode(song);
-        newHead.next = head;
-        head = newHead;
-    }
-
-    public int currentsize() {
-        return size;
+        MRU.next = newHead;
+        MRU = MRU.next;
     }
 
     public void print() {
-        SongNode temp = head;
+        SongNode temp = LRU;
         while (temp != null) {
             System.out.print(temp.data);
             if (temp.next != null) {
@@ -76,27 +77,16 @@ class Spotify {
         }
         System.out.println();
     }
-
-    public void clearPlaylist() {
-        head = null;
-        size = 0;
-        return;
-    }
 }
 
 public class LRU_singly {
     public static void main(String[] args) {
         Spotify playlist = new Spotify(4);
         playlist.insert("Ae Dil Hain MushKil");
-        System.out.println(playlist.currentsize());
         playlist.insert("Jogi Ji Dheere Dheere");
-        System.out.println(playlist.currentsize());
         playlist.insert("Bulleya");
-        System.out.println(playlist.currentsize());
         playlist.insert("Jai Jai Shiv Shankar");
-        System.out.println(playlist.currentsize());
         playlist.insert("Jogi Ji Dheere Dheere");
-        System.out.println(playlist.currentsize());
         playlist.print();
     }
 }
