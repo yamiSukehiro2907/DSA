547c8c4249dee4bd8a00e4f6694031fe32049688
Vimal Kumar Yadav <vimalyadavkr001@gmail.com>
2025-03-20 10:59:32 +0530

    Stack

---
+++ L23/using_Stack.java
@@ -0,0 +1,10 @@
+package L23;
+
+import java.util.Stack;
+
+public class using_Stack {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        
+    }
+}
