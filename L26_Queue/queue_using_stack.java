a1a8030af5f28a18a6311889b17b39eba887f267
Vimal Kumar Yadav <vimalyadavkr001@gmail.com>
2025-03-28 17:54:33 +0530

    Binary Tree Class 1 completed

--- L26_Queue/queue_using_stack.java
+++ L26_Queue/queue_using_stack.java
@@ -3,23 +3,39 @@ package L26_Queue;
 import java.util.*;
 
 public class queue_using_stack {
-    // 6 7 8 6 5 6
-    public static void main(String[] args) {
-        class Queue {
-            private Stack<Integer> main;
+    static class queue {
+        private Stack<Integer> reverse;
+        private Stack<Integer> rightOrder;
 
-            Queue() {
-                this.main = new Stack<>();
-            }
-
-            public void add(int val) {
-                main.push(val);
-            }
-            public int remove(){
-                Stack<Integer> temp = new Stack<>();
-                while(!main.isEmpty()) temp.push(main.pop());
-                int value = temp.pop();
-            }
+        queue() {
+            this.reverse = new Stack<>();
+            this.rightOrder = new Stack<>();
         }
+
+        public void add(int val) {
+            reverse.push(val);
+        }
+
+        public int remove() {
+            if (reverse.isEmpty() && rightOrder.isEmpty())
+                throw new RuntimeException("Queue is Empty!");
+            if (rightOrder.isEmpty()) {
+                while (!reverse.isEmpty())
+                    rightOrder.push(reverse.pop());
+            }
+            return rightOrder.pop();
+        }
+    }
+
+    public static void main(String[] args) {
+        queue q = new queue();
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        q.add(6);
+        q.add(5);
+        q.add(6);
+        System.out.println(q.remove());
+        System.out.println(q.remove());
     }
 }
