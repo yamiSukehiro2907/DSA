918dd5ee8185049d1d204f0ab6160bf6e5cc1985
Vimal Kumar Yadav <vimalyadavkr001@gmail.com>
2025-04-04 23:04:39 +0530

    april 04

---
+++ L29_BinaryTree/TreeNode.java
@@ -0,0 +1,11 @@
+package L29_BinaryTree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
---
+++ L30_BinaryTree/TreeNode.java
@@ -0,0 +1,12 @@
+package L30_BinaryTree;
+
+public class TreeNode {
+    TreeNode left;
+    TreeNode right;
+    int val;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
