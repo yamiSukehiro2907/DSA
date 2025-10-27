67959841de6fd0542335eea8258bb305fd1443fe
Vimal290704 <vimalyadavkr001@gmail.com>
2025-10-27 11:51:21 +0530

    prog: fixed names of binary_tree folder

---
+++ binary_tree/revision/find_diameter_in_binary_tree.java
@@ -0,0 +1,13 @@
+package revision;
+import common.TreeNode;
+
+public class find_diameter_in_binary_tree {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static int find_diameter(TreeNode root){
+        return 0;
+    }
+}
